a0d869433e32130fa4d191acbcb7f87521e114b2
Nikolay Yarunin <nik.yarunin.dev@gmail.com>
2024-01-31 17:48:28 -0500

    Exercise 3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,8 +3,10 @@ const morgan = require('morgan')
 
 const app = express()
 
+
+morgan.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : ' ')
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
