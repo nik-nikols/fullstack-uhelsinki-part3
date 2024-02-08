0d95ab0da768b249bb7469c29714751c5c2229bb
Nikolay Yarunin <nik.yarunin.dev@gmail.com>
2024-02-08 12:54:14 -0500

    Exercise 3.18

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -12,45 +12,15 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-    let maxId = Math.round((Math.random() * 10000))
-
-    while (persons.find(person => person.id === maxId)) {
-        maxId = Math.round((Math.random() * 10000))
-    }
-
-    return maxId
-}
 
 app.get('/info', (request, response) => {
-    const dateNow = new Date(Date.now())
-    const reply = `<p>Phonebook has info for ${persons.length} people</p><p>${dateNow}</p>`
-
-    response.send(reply)
-})
+    Person.find({})
+    .then(people => {
+        const dateNow = new Date(Date.now());
+        const reply = `<p>Phonebook has info for ${people.length} people</p><p>${dateNow}</p>`
+            response.send(reply)
+    });
+});
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
